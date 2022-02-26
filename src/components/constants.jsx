93f4382e9ff7b591e3384a42be28b0a9e16b213f
Rebecca Key <rek990@gmail.com>
2022-02-26 08:43:25 -0500

    Created constants for band, frequency, and mode.

---
+++ src/components/constants.jsx
@@ -0,0 +1,45 @@
+const BAND = [
+  {
+    id: 0,
+    band: "160m",
+  },
+  { id: 1, band: "80m" },
+  { id: 2, band: "40m" },
+  { id: 3, band: "30m" },
+  { id: 4, band: "20m" },
+  { id: 5, band: "17m" },
+  { id: 6, band: "15m" },
+  { id: 7, band: "12m" },
+  { id: 8, band: "10m" },
+  { id: 9, band: "6m" },
+  { id: 10, band: "2m" },
+  { id: 11, band: "70cm" },
+];
+
+const MODE = [
+  {
+    id: 0,
+    mode: "SSB",
+  },
+  { id: 1, mode: "CW" },
+  { id: 2, mode: "AM" },
+  { id: 3, mode: "FM" },
+  { id: 4, mode: "RTTY" },
+  { id: 5, mode: "PSK31" },
+  { id: 6, mode: "DSTAR" },
+];
+
+const FREQUENCY = [
+  { id: 0, frequency: "1.8 MHz" },
+  { id: 1, frequency: "3.5 MHz" },
+  { id: 2, frequency: "7 MHz" },
+  { id: 3, frequency: "10.1 MHz" },
+  { id: 4, frequency: "14 MHz" },
+  { id: 5, frequency: "18 MHz" },
+  { id: 6, frequency: "21 MHz" },
+  { id: 7, frequency: "24 MHz" },
+  { id: 8, frequency: "28 MHz" },
+  { id: 10, frequency: "50 MHz" },
+  { id: 11, frequency: "144 MHz" },
+  { id: 12, frequency: "420 MHz" },
+];
